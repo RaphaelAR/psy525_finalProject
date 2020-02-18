2dbd29d70300e169f3ddd1776ba006db996461d9
RaphaelAR <rrodriguez@co-impact.co>
2020-02-17 21:20:32 -0500

    first final project idea

--- psy525_rodriguez_180220_finalProjectSummary.r
+++ psy525_rodriguez_180220_finalProjectSummary.r
@@ -6,3 +6,13 @@ output:
 author: "Raphael A Rodriguez"
 date: "`r Sys.time()`"
 ---
+
+# Final Project
+
+## Possible Ideas
+1. Visualization of Gender Bias & Capitalism
+* Implicit Association Test data on Gender-Career biases - do people associate career with men and family with women
+* Implicit Association Test data on Gender-Science biases - do people associate science with women or men more
+* Economic Freedom Index (Capitalism measure)
+* Democracy Index
+I would like to create a visualization of these 4 different datasets (or a combination thereof) to identify potential correlations between gender biases, capitalism and democracy. Ideally I would build machine learning models with this data but open to using other visual tools established in R.
